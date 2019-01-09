2f009707b597f01e1ac47911c56b1acdcbf542e0
fazrul <fazrul11@hotmail.com>
2019-01-09 18:16:17 +0800

    New form voucher

---
+++ resources/views/finance/layouts/finance.blade.php
@@ -0,0 +1,9 @@
+
+
+@section('styles')
+    {{ Html::style(mix('assets/admin/css/admin.css')) }}
+@endsection
+
+@section('scripts')
+    {{ Html::script(mix('assets/admin/js/admin.js')) }}
+@endsection
