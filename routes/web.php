bb46e1dfa91303feec7786cb497c470e74e48889
fazrul <fazrul11@hotmail.com>
2019-03-04 12:32:29 +0800

    generate multiple gl code pdf

--- routes/web.php
+++ routes/web.php
@@ -97,13 +97,13 @@ Route::group(['prefix' => 'finance', 'as' => 'finance.', 'namespace' => 'Finance
     {
         $voucherdetail = DB::table('cash_voucher')->where('id', '=', $voucherid)->get();
         $cost_centre = DB::table('cost_centre')->where('id', '=', $voucherdetail[0]->cost_centre)->value('cost_centre');
-        // $glcode_str = explode(',', $voucherdetail[0]->gl_code);
-        // $glcode_all = '';
-        // for ($i=0; $i < count($glcode_str); $i++) {
-        //   if ($i > 0) { $glcode_all .= ","; }
-        //   $gl_code = DB::table('gl_code')->where('id', '=', $glcode_str[$i])->value('gl_code');
-        //   $glcode_all .= $gl_code;
-        // }
+        $glcode_str = explode(',', $voucherdetail[0]->gl_code);
+        $glcode_all = '';
+        for ($i=0; $i < count($glcode_str); $i++) {
+          if ($i > 0) { $glcode_all .= ", "; }
+          $gl_code = DB::table('gl_code')->where('id', '=', $glcode_str[$i])->value('gl_code');
+          $glcode_all .= $gl_code;
+        }
         // $gl_code = DB::table('gl_code')->where('id', '=', $voucherdetail[0]->gl_code)->value('gl_code');
 
         $copytitle = ['Finance\'s Copy', 'Customer\'s Copy'];
@@ -142,18 +142,10 @@ Route::group(['prefix' => 'finance', 'as' => 'finance.', 'namespace' => 'Finance
             Fpdf::Cell(50, 8, 'DATE: '.date("d/m/Y",strtotime($voucherdetail[0]->created_date)), $border);
             Fpdf::SetXY(150, 46+$lastY);
             Fpdf::Cell(50, 8, 'Cost Centre: '.$cost_centre, 1);
-            $glY = 54;
-            $glcode_str = explode(',', $voucherdetail[0]->gl_code);
-            $glcode_all = '';
-            for ($i=0; $i < count($glcode_str); $i++) {
-              if ($i > 0) { $glcode_all .= ","; }
-              $gl_code = DB::table('gl_code')->where('id', '=', $glcode_str[$i])->value('gl_code');
-              $glcode_all .= $gl_code;
-            }
-            Fpdf::SetXY(150, $glY+$lastY);
-            Fpdf::Cell(50, 8, 'GL Code: '.$voucherdetail[0]->gl_code, 1);
-            Fpdf::SetXY(150, ($glY+8)+$lastY);
-            Fpdf::MultiCell(50, 8, "Cheque Signed by:\n ", 1);
+            Fpdf::SetXY(150, 54+$lastY);
+            Fpdf::MultiCell(50, 5, 'GL Code: '.$glcode_all, 1, 'left');
+            Fpdf::SetXY(150, 69+$lastY);
+            Fpdf::MultiCell(50, 7, "Cheque Signed by:\n ", 1);
             Fpdf::SetXY(150, 84+$lastY);
             Fpdf::MultiCell(50, 8, 'RM: '.$voucherdetail[0]->rm, 1);
             // bottom
@@ -212,7 +204,7 @@ Route::group(['prefix' => 'mcr', 'as' => 'mcr.', 'namespace' => 'Mcr', 'middlewa
             Fpdf::MultiCell(130, 8, 'Pay to '.$voucherdetail[0]->pay_to, $border);
             Fpdf::SetY(68+$lastY);
             Fpdf::MultiCell(130, 8, 'Being payment for '.$voucherdetail[0]->payment_for, $border, 'L');
-            Fpdf::SetY(84+$lastY);
+            Fpdf::SetY(86+$lastY);
             Fpdf::MultiCell(130, 8, 'Ringgit '.$voucherdetail[0]->ringgit, $border, 'L');
             // right
             Fpdf::SetXY(150, 10+$lastY);
@@ -228,7 +220,7 @@ Route::group(['prefix' => 'mcr', 'as' => 'mcr.', 'namespace' => 'Mcr', 'middlewa
             Fpdf::Cell(50, 8, 'GL Code: '.$gl_code, 1);
             Fpdf::SetXY(150, 62+$lastY);
             Fpdf::MultiCell(50, 8, "Cheque Signed by:\n ", 1);
-            Fpdf::SetXY(150, 84+$lastY);
+            Fpdf::SetXY(150, 86+$lastY);
             Fpdf::MultiCell(50, 8, 'RM: '.$voucherdetail[0]->rm, 1);
             // bottom
             Fpdf::SetY(100+$lastY);
